7b6c79a114232bc10b0b31e9499412fd25b35014
fbqudrlf09 <fbqudrlf09@naver.com>
2024-11-14 10:12:43 +0900

    first commit

---
+++ src/main/java/com/example/taskmanagerdev/TaskManagerDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagerdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerDevApplication.class, args);
+    }
+
+}
